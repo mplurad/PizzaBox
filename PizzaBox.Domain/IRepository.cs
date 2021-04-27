3de68cbe27f36cffbd1c5b72c715fef4f4fd14a3
Miles Plurad <pluradmiles@gmail.com>
2021-04-27 13:36:58 -0500

    Updated IRepository.cs and Repository.cs

--- PizzaBox.Domain/IRepository.cs
+++ PizzaBox.Domain/IRepository.cs
@@ -5,29 +5,47 @@ namespace PizzaBox.Domain
 {
     public interface IRepository
     {
-        // ADD
+        // GET ALL
+        List<ACustomer> GetAllCustomers();
+        List<AStore> GetAllStores();
+        List<AOrder> GetAllOrders();
+        List<APizzaSize> GetAllPizzaSizes();
+        List<ACrust> GetAllCrusts();
+        List<ATopping> GetAllToppings();
+        List<APizza> GetAllPizzas();
+        List<APizzaTopping> GetAllPizzaToppings();
+
+        // GET BY ID
+        ACustomer GetCustomers(int id);
+        AStore GetStore(int id);
+        AOrder GetOrder(int id);
+        APizzaSize GetPizzaSize(int id);
+        ACrust GetCrust(int id);
+        ATopping GetTopping(int id);
+        APizza GetPizza(int id);
+        APizzaTopping GetPizzaTopping(int id);
+
+        // POST
         void AddCustomer(ACustomer customer);
         void AddStore(AStore store);
         void AddOrder(AOrder order);
         void AddPizza(APizza pizza);
+        void AddPizzaSize(APizzaSize pizzaSize);
+        void AddTopping(ATopping topping);
+        void AddCrust(ACrust crust);
         void AddPizzaTopping(APizzaTopping pizzaTopping);
 
-        // GET
-        List<ACustomer> GetAllCustomers();
-        List<AStore> GetAllStores();
-        List<AOrder> GetAllOrders();
-        List<APizza> GetAllPizzas();
-        List<ACrust> GetAllCrusts();
-        ACrust GetCrust(int Id);
-        List<APizzaSize> GetAllPizzaSizes();
-        List<ATopping> GetAllToppings();
-        List<APizzaTopping> GetAllPizzaToppings();
+        // PUT
+        void UpdateCustomer(ACustomer customer);
+        void UpdateStore(AStore store);
+        void UpdateOrder(ACustomer customer);
+        void UpdatePizzaSize(AStore store);
+        void UpdateCrust(ACustomer customer);
+        void UpdateTopping(AStore store);
+        void UpdatePizza(ACustomer customer);
+        void UpdatePizzaTopping(AStore store);
 
-        // UPDATE
-        void Update(ACustomer customer);
-        void Update(AStore store);
-
-        // DELETE
+        // DELETE BY ID
         void DeleteStore(byte storeID);
     }
 }
--- PizzaBox.Storing/Repository.cs
+++ PizzaBox.Storing/Repository.cs
@@ -15,6 +15,90 @@ namespace PizzaBox.Storing
             this.context = context;
         }
 
+        // GET ALL
+        public List<ACustomer> GetAllCustomers()
+        {
+            var customers = context.Customers;
+            return customers.Select(customer => mapper.Map(customer)).ToList();
+        }
+
+        public List<AStore> GetAllStores()
+        {
+            var stores = context.Stores;
+            return stores.Select(store => mapper.Map(store)).ToList();
+        }
+
+        public List<AOrder> GetAllOrders()
+        {
+            return context.Orders.Select(mapper.Map).ToList();
+        }
+
+        public List<APizzaSize> GetAllPizzaSizes()
+        {
+            return context.PizzaSizes.Select(mapper.Map).ToList();
+        }
+
+        public List<ACrust> GetAllCrusts()
+        {
+            return context.Crusts.Select(mapper.Map).ToList();
+        }
+
+        public List<ATopping> GetAllToppings()
+        {
+            return context.Toppings.Select(mapper.Map).ToList();
+        }
+
+        public List<APizza> GetAllPizzas()
+        {
+            return context.Pizzas.Select(mapper.Map).ToList();
+        }
+
+        public List<APizzaTopping> GetAllPizzaToppings()
+        {
+            return context.PizzaToppings.Select(mapper.Map).ToList();
+        }
+
+        // GET BY ID
+        public ACustomer GetCustomer(int id)
+        {
+            return mapper.Map(context.Customers.Where(x => x.CustomerId == id).FirstOrDefault());
+        }
+
+        public AStore GetStore(int id)
+        {
+            return mapper.Map(context.Stores.Where(x => x.StoreId == id).FirstOrDefault());
+        }
+
+        public AOrder GetOrder(int id)
+        {
+            return mapper.Map(context.Orders.Where(x => x.OrderId == id).FirstOrDefault());
+        }
+
+        public APizzaSize GetPizzaSize(int id)
+        {
+            return mapper.Map(context.PizzaSizes.Where(x => x.PizzaSizeId == id).FirstOrDefault());
+        }
+
+        public ACrust GetCrust(int id)
+        {
+            return mapper.Map(context.Crusts.Where(x => x.CrustId == id).FirstOrDefault());
+        }
+
+        public ATopping GetTopping(int id)
+        {
+            return mapper.Map(context.Toppings.Where(x => x.ToppingId == id).FirstOrDefault());
+        }
+
+        public APizza GetPizza(int id)
+        {
+            return mapper.Map(context.Pizzas.Where(x => x.PizzaId == id).FirstOrDefault());
+        }
+
+        public APizzaTopping GetPizzaTopping(int id)
+        {
+            return mapper.Map(context.PizzaToppings.Where(x => x.PizzaToppingId == id).FirstOrDefault());
+        }
+
         // POST
         public void AddCustomer(ACustomer customer)
         {
@@ -33,74 +117,46 @@ namespace PizzaBox.Storing
             context.Add(mapper.Map(order));
             context.SaveChanges();
         }
+
+        public void AddPizzaSize(APizzaSize pizzaSize)
+        {
+            context.Add(mapper.Map(pizzaSize));
+            context.SaveChanges();
+        }
+
+        public void AddCrust(ACrust crust)
+        {
+            context.Add(mapper.Map(crust));
+            context.SaveChanges();
+        }
+
+        public void AddTopping(ATopping topping)
+        {
+            context.Add(mapper.Map(topping));
+            context.SaveChanges();
+        }
+
         public void AddPizza(APizza pizza)
         {
             context.Add(mapper.Map(pizza));
             context.SaveChanges();
         }
+
         public void AddPizzaTopping(APizzaTopping pizzaTopping)
         {
             context.Add(mapper.Map(pizzaTopping));
             context.SaveChanges();
         }
 
-        // GET
-        public List<ACustomer> GetAllCustomers()
-        {
-            var customers = context.Customers;
-            return customers.Select(customer => mapper.Map(customer)).ToList();
-        }
-
-        public List<AStore> GetAllStores()
-        {
-            var stores = context.Stores;
-            return stores.Select(store => mapper.Map(store)).ToList();
-        }
-
-        public List<AOrder> GetAllOrders()
-        {
-            return context.Orders.Select(mapper.Map).ToList();
-        }
-
-        public List<APizza> GetAllPizzas()
-        {
-            return context.Pizzas.Select(mapper.Map).ToList();
-        }
-
-        public List<ACrust> GetAllCrusts()
-        {
-            return context.Crusts.Select(mapper.Map).ToList();
-        }
-        public ACrust GetCrust(int Id)
-        {
-            return mapper.Map(context.Crusts.Where(crust => crust.CrustId == Id).FirstOrDefault());
-        }
-
-        public List<APizzaSize> GetAllPizzaSizes()
-        {
-            return context.PizzaSizes.Select(mapper.Map).ToList();
-        }
-
-        public List<ATopping> GetAllToppings()
-        {
-            return context.Toppings.Select(mapper.Map).ToList();
-        }
-
-        public List<APizzaTopping> GetAllPizzaToppings()
-        {
-            return context.PizzaToppings.Select(mapper.Map).ToList();
-        }
-
         // PUT
-        public void Update(ACustomer customer)
+        public void UpdateCustomer(ACustomer customer)
         {
-            var toUpdate = context.Customers.Where(c => c.CustomerId == customer.CustomerId).FirstOrDefault();
+            var toUpdate = GetCustomer(customer.CustomerId);
             if (toUpdate != null)
             {
                 toUpdate.CustomerFirstName = customer.CustomerFirstName;
                 toUpdate.CustomerLastName = customer.CustomerLastName;
                 toUpdate.CustomerPhone = customer.CustomerPhone;
-                toUpdate.Orders = customer.Orders.Select(mapper.Map).ToList();
                 toUpdate.Password = customer.Password;
                 toUpdate.Username = customer.Username;
                 toUpdate.CustomerCardNumber = customer.CustomerCardNumber;
@@ -111,12 +167,82 @@ namespace PizzaBox.Storing
             }
         }
 
-        public void Update(AStore store)
+        public void UpdateStore(AStore newItem)
         {
-            var toUpdate = context.Stores.Where(s => s.StoreId == store.StoreId).FirstOrDefault();
-            if (toUpdate != null)
+            var oldItem = GetStore(newItem.StoreId);
+            if (oldItem != null)
             {
-                toUpdate.StoreLocation = store.StoreLocation;
+                oldItem.StoreLocation = newItem.StoreLocation;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateOrder(AOrder newItem)
+        {
+            var oldItem = GetOrder(newItem.OrderId);
+            if (oldItem != null)
+            {
+                oldItem.Cost = newItem.Cost;
+                oldItem.CustomerId = newItem.CustomerId;
+                oldItem.OrderDate = newItem.OrderDate;
+                oldItem.StoreId = newItem.StoreId;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdatePizzaSize(APizzaSize newItem)
+        {
+            var oldItem = GetPizzaSize(newItem.PizzaSizeId);
+            if (oldItem != null)
+            {
+                oldItem.PizzaSizeInches = newItem.PizzaSizeInches;
+                oldItem.PizzaSizeName = newItem.PizzaSizeName;
+                oldItem.PizzaSizePrice = newItem.PizzaSizePrice;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCrust(ACrust newItem)
+        {
+            var oldItem = GetCrust(newItem.CrustId);
+            if (oldItem != null)
+            {
+                oldItem.CrustName = newItem.CrustName;
+                oldItem.CrustPrice = newItem.CrustPrice;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateTopping(ATopping newItem)
+        {
+            var oldItem = GetTopping(newItem.ToppingId);
+            if (oldItem != null)
+            {
+                oldItem.ToppingName = newItem.ToppingName;
+                oldItem.ToppingPrice = newItem.ToppingPrice;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdatePizza(APizza newItem)
+        {
+            var oldItem = GetPizza(newItem.PizzaId);
+            if (oldItem != null)
+            {
+                oldItem.CrustId = newItem.CrustId;
+                oldItem.OrderId = newItem.OrderId;
+                oldItem.PizzaPrice = newItem.PizzaPrice;
+                oldItem.PizzaSizeId = newItem.PizzaSizeId;
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdatePizza(newItem)
+        {
+            var oldItem = GetStore(newItem.StoreId);
+            if (oldItem != null)
+            {
+                oldItem.StoreLocation = newItem.StoreLocation;
                 context.SaveChanges();
             }
         }
